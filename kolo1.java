f7b98218e41bb3a1019495fb581d66cca944ba63
Andrzej Stolarczyk <andrewspec@live.com>
2011-06-01 14:59:57 +0200

    odpowiedziano na pytania

--- kolo1.java
+++ kolo1.java
@@ -3,7 +3,16 @@
 class Kolo1 {
   public static void main(String[] args) {
     System.out.println("I don't like git");
+	pytanie();
   }
+
+	public static void pytanie(){
+		System.out.println("Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?");
+	System.out.println("A1) git diff HEAD^^ HEAD^^^");
+	System.out.println("Q2) Co sie zmienilo?");
+	System.out.println("A2) Zmieni³ siê pkt pierwszy";
+}
+
 
 }
 
